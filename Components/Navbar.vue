11ef20cb3a73691100e043b9cb5e67ad5de3aa9d
Akshata9986 <akshatanaik9164@gmail.com>
2022-10-04 09:20:12 +0530

    nuxt_routing

---
+++ Components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <NuxtLink to="/" class="navbar-brand">Navbar</NuxtLink>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <NuxtLink to="/" class="nav-link"
+            >Home <span class="sr-only">(current)</span>
+          </NuxtLink>
+        </li>
+        <li class="nav-item">
+          <NuxtLink to="/login" class="nav-link">Login</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav-link {
+  cursor: pointer;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+export default defineNuxtConfig({
+    // head: {
+    //     script: [
+    //         {
+    //             src: "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+    //             integrity: "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN",
+    //             crossorigin: "anonymous"
+    //         }
+    //     ],
+    //     link: [
+    //         {
+    //             rel: 'stylesheet',
+    //             href: "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css",
+    //             integrity: "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm",
+    //             crossorigin: "anonymous"
+    //         }
+    //     ]
+    // }
+})
+
